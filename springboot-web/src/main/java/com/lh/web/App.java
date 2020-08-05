28c8527d2b2d812adcb7967b9aa444dc2a428898
lihao3 <lihao3@wison.com>
2020-08-05 13:01:53 +0800

    测试自动构建

--- springboot-web/src/main/java/com/lh/web/App.java
+++ springboot-web/src/main/java/com/lh/web/App.java
@@ -4,6 +4,9 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author lihao3
+ */
 @MapperScan("com.lh.bean.entity")
 @SpringBootApplication
 public class App {
