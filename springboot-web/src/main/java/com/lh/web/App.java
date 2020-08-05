8f2485ae925ed9a572ee48db0f500f91ab460441
lihao3 <lihao3@wison.com>
2020-08-05 12:44:14 +0800

    修改dockerFile文件

--- springboot-web/src/main/java/com/lh/web/App.java
+++ springboot-web/src/main/java/com/lh/web/App.java
@@ -1,9 +1,10 @@
 package com.lh.web;
 
-
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.lh.bean.entity")
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
