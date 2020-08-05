c82589e4332e0400e58056dda3611db1334a7252
lihao3 <lihao3@wison.com>
2020-08-05 13:09:46 +0800

    测试自动构建

--- springboot-web/src/main/java/com/lh/web/App.java
+++ springboot-web/src/main/java/com/lh/web/App.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
+ * 测试
  * @author lihao3
  */
 @MapperScan("com.lh.bean.entity")
